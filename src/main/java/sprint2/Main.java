91ea4901fb28c9547738f71e70f4e9c23232aa04
Stanislav Garkushov <stanislav.garkushov@simbirsoft.com>
2025-07-30 17:06:50 +0300

    Добавить константы цыета яблок при создении объекта

--- src/main/java/sprint2/Main.java
+++ src/main/java/sprint2/Main.java
@@ -3,6 +3,7 @@ package sprint2;
 import sprint2.model.Apple;
 import sprint2.model.Food;
 import sprint2.model.Meat;
+import sprint2.model.constants.Colour;
 import sprint2.service.ShoppingCart;
 
 import static sprint2.model.constants.Amount.*;
@@ -12,8 +13,8 @@ public class Main {
 
     public static void main(String[] args) {
         Food meat = new Meat(amount_5, price_100);
-        Food appleRed = new Apple(amount_10, price_50, "red");
-        Food appleGreen = new Apple(amount_8, price_60, "green");
+        Food appleRed = new Apple(amount_10, price_50, Colour.RED);
+        Food appleGreen = new Apple(amount_8, price_60, Colour.GREEN);
         Food[] listFood = {meat, appleRed, appleGreen};
         ShoppingCart shoppingCart = new ShoppingCart(listFood);
         System.out.println(shoppingCart.getTotalPriceWithoutDiscount());
